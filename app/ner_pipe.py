a04371a1dca026c3e1d7b8c7c39eec7226ff1c2b
crodriguez1a <crodriguez1a@gmail.com>
2019-06-22 18:07:49 -0400

    Boilerplate - custom entity training

--- app/ner_pipe.py
+++ app/ner_pipe.py
@@ -13,8 +13,8 @@ def load_text(path:str) -> str:
 
 
 
-
-
+# https://towardsdatascience.com/custom-named-entity-recognition-using-spacy-7140ebbb3718
+# http://datacamp-community-prod.s3.amazonaws.com/29aa28bf-570a-4965-8f54-d6a541ae4e06
 
 if __name__ == "__main__":
 
@@ -30,5 +30,5 @@ if __name__ == "__main__":
     named_entities:list = [(ent.text, ent.label_) for ent in doc.ents]
     print(named_entities)
 
-    visual_dependencies:str = spacy.displacy.render(doc, style="dep")
-    print(visual_dependencies)
+    # visual_dependencies:str = spacy.displacy.render(doc, style="dep")
+    # print(visual_dependencies)
